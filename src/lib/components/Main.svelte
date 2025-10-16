b51426fd47adacb6dd69d6b2457af5d9e7495c7e
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-16 14:04:11 +0800

    Begin Frontend

---
+++ src/lib/components/Main.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Heading, DarkMode } from 'flowbite-svelte';
+	import { RadioSelect } from './UI';
+	let PF = $state('polished');
+</script>
+
+<div class="m-5">
+	<div class="flex justify-between">
+		<Heading tag="h1">Polished Editor</Heading>
+		<div class="flex items-center gap-3">
+			<RadioSelect
+				bind:value={PF}
+				options={[
+					{ text: 'Polished', id: 'polished' },
+					{ text: 'Faithful', id: 'faithful' }
+				]}
+			/>
+			<DarkMode class="shrink-0 border border-gray-300 dark:border-gray-800" />
+		</div>
+	</div>
+</div>
---
+++ src/lib/components/UI/RadioSelect.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { ButtonGroup, RadioButton } from 'flowbite-svelte';
+	let { value = $bindable(), options, onchange = () => {} } = $props();
+</script>
+
+<ButtonGroup>
+	{#each options as { text, id }}
+		<RadioButton
+			outline
+			value={id}
+			class="border-gray-300"
+			checkedClass="bg-purple-600 text-white dark:bg-purple-600 dark:text-white hover:bg-purple-600 hover:text-white dark:hover:bg-purple-600 dark:hover:text-white"
+			bind:group={value}
+			onchange={onchange?.()}>{text}</RadioButton
+		>
+	{/each}
+</ButtonGroup>
