48e89d5af837968aae52bdafc978ac0b3eee7d00
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-21 20:17:31 +0800

    Seperate Player checksum from Party, apply at end

--- src/lib/parsers/party/reverse/reverseParseParty.ts
+++ src/lib/parsers/party/reverse/reverseParseParty.ts
@@ -5,22 +5,21 @@ import type { PartyMon } from '$lib/types';
 import reverseParsePartyMon from './reverseParsePartyMon';
 
 function reverseParseParty(
-	fileHex: string[],
-	party: PartyMon[],
-	PF: 'polished' | 'faithful'
+  fileHex: string[],
+  party: PartyMon[],
+  PF: 'polished' | 'faithful'
 ): string[] {
-	const address = addresses.sPokémonData + 8;
-	fileHex[address - 8] = party.filter(Boolean).length.toString(16).padStart(2, '0');
-	for (let i = 0; i < 6; i++) {
-		if (!party[i]) continue;
-		fileHex = reverseParsePartyMon(fileHex, address + 48 * i, party[i], PF);
-		fileHex = writeString(fileHex, address + 288 + i * 11, 8, party[i].OTNickname, false);
-		fileHex[address + 307 + i * 11] = bin2hex(
-			party[i].hyperTraining.map((stat) => (stat ? '1' : '0')).join('')
-		);
-		fileHex = writeString(fileHex, address + 354 + i * 11, 11, party[i].nickname, false);
-	}
-	fileHex = checksumPlayer(fileHex);
-	return fileHex;
+  const address = addresses.sPokémonData + 8;
+  fileHex[address - 8] = party.filter(Boolean).length.toString(16).padStart(2, '0');
+  for (let i = 0; i < 6; i++) {
+    if (!party[i]) continue;
+    fileHex = reverseParsePartyMon(fileHex, address + 48 * i, party[i], PF);
+    fileHex = writeString(fileHex, address + 288 + i * 11, 8, party[i].OTNickname, false);
+    fileHex[address + 307 + i * 11] = bin2hex(
+      party[i].hyperTraining.map((stat) => (stat ? '1' : '0')).join('')
+    );
+    fileHex = writeString(fileHex, address + 354 + i * 11, 11, party[i].nickname, false);
+  }
+  return fileHex;
 }
 export default reverseParseParty;
--- src/lib/parsers/reverseParseSave.ts
+++ src/lib/parsers/reverseParseSave.ts
@@ -1,21 +1,23 @@
 import { buf2hex, hex2buf } from '$lib/utils';
 import type { BagSlot, Box, PartyMon, Player } from '../types';
 import reverseParseBoxes from './boxes/reverse/reverseParseBoxes';
+import checksumPlayer from './checksumPlayer';
 import reverseParseParty from './party/reverse/reverseParseParty';
 
 async function reverseParseSave(
-	file: File,
-	party: PartyMon[],
-	boxes: Box[],
-	bag: Record<string, BagSlot>,
-	player: Player,
-	PF: 'polished' | 'faithful'
+  file: File,
+  party: PartyMon[],
+  boxes: Box[],
+  bag: Record<string, BagSlot>,
+  player: Player,
+  PF: 'polished' | 'faithful'
 ): Promise<ArrayBuffer> {
-	let fileHex = await buf2hex(file);
+  let fileHex = await buf2hex(file);
 
-	fileHex = reverseParseParty(fileHex, party, PF);
-	fileHex = reverseParseBoxes(fileHex, boxes, PF);
+  fileHex = reverseParseParty(fileHex, party, PF);
+  fileHex = reverseParseBoxes(fileHex, boxes, PF);
+  fileHex = checksumPlayer(fileHex)
 
-	return hex2buf(fileHex);
+  return hex2buf(fileHex);
 }
 export default reverseParseSave;
