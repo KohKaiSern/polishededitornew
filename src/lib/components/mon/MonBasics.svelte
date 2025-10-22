a71c24b2b7b2f63575df3c8b0d166a18b9bb4af3
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-22 22:29:46 +0800

    Fix overlapping delete button

--- src/lib/components/mon/MonBasics.svelte
+++ src/lib/components/mon/MonBasics.svelte
@@ -6,15 +6,11 @@
 	let { mon = $bindable(), PF }: { mon: Mon | PartyMon; PF: 'polished' | 'faithful' } = $props();
 </script>
 
-<div class="flex flex-col gap-5">
-	<section>
-		<Heading tag="h6" class="mb-5">Nickname</Heading>
-		<TextInput bind:value={mon.nickname} maxLength={10} />
-	</section>
-	<Heading tag="h6">Species & Form</Heading>
-	<Heading tag="h6">Held Item</Heading>
-	<Heading tag="h6">Ability</Heading>
-	<Heading tag="h6">Level</Heading>
-	<Heading tag="h6">Status</Heading>
-	<Heading tag="h6">Current HP</Heading>
-</div>
+<Heading tag="h6" class="mb-5">Nickname</Heading>
+<TextInput bind:value={mon.nickname} maxLength={10} />
+<Heading tag="h6">Species & Form</Heading>
+<Heading tag="h6">Held Item</Heading>
+<Heading tag="h6">Ability</Heading>
+<Heading tag="h6">Level</Heading>
+<Heading tag="h6">Status</Heading>
+<Heading tag="h6">Current HP</Heading>
--- src/lib/components/mon/MonEditor.svelte
+++ src/lib/components/mon/MonEditor.svelte
@@ -10,31 +10,29 @@
 	}: { mon: PartyMon | Mon; PF: 'polished' | 'faithful'; onDelete: () => void } = $props();
 </script>
 
-<div class="relative h-full flex flex-col">
-	<Heading tag="h3" class="p-2">Edit {mon.nickname}</Heading>
-	<Accordion class="mt-4">
-		<AccordionItem open>
-			{#snippet header()}Basics{/snippet}
-			<MonBasics bind:mon {PF} />
-		</AccordionItem>
-		<AccordionItem>
-			{#snippet header()}Stats{/snippet}
-			<div>TODO</div>
-		</AccordionItem>
-		<AccordionItem>
-			{#snippet header()}Moveset{/snippet}
-			<div>TODO</div>
-		</AccordionItem>
-		<AccordionItem>
-			{#snippet header()}Miscellaneous{/snippet}
-			<div>TODO</div>
-		</AccordionItem>
-		<AccordionItem>
-			{#snippet header()}OT & Caught Data{/snippet}
-			<div>TODO</div>
-		</AccordionItem>
-	</Accordion>
-	<div class="absolute bottom-0 right-0">
-		<Button color="red" onclick={onDelete}>Delete Pokémon</Button>
-	</div>
+<Heading tag="h3" class="p-2">Edit {mon.nickname}</Heading>
+<Accordion class="mt-4">
+	<AccordionItem open>
+		{#snippet header()}Basics{/snippet}
+		<MonBasics bind:mon {PF} />
+	</AccordionItem>
+	<AccordionItem>
+		{#snippet header()}Stats{/snippet}
+		<div>TODO</div>
+	</AccordionItem>
+	<AccordionItem>
+		{#snippet header()}Moveset{/snippet}
+		<div>TODO</div>
+	</AccordionItem>
+	<AccordionItem>
+		{#snippet header()}Miscellaneous{/snippet}
+		<div>TODO</div>
+	</AccordionItem>
+	<AccordionItem>
+		{#snippet header()}OT & Caught Data{/snippet}
+		<div>TODO</div>
+	</AccordionItem>
+</Accordion>
+<div class="mt-5 ml-auto">
+	<Button color="red" onclick={onDelete}>Delete Pokémon</Button>
 </div>
