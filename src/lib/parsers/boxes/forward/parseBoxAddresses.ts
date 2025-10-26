594dfc13140476e75b45e1751d0edf8dc22d3f0a
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-27 04:41:55 +0800

    Forgot to reverseParse forms, somehow

--- src/lib/parsers/boxes/forward/parseBoxAddresses.ts
+++ src/lib/parsers/boxes/forward/parseBoxAddresses.ts
@@ -1,62 +1,62 @@
 import addresses from '$data/addresses.json';
 
 function parseBoxAddresses(fileHex: string[]): number[][] {
-	const indexes = parseIndexes(fileHex);
-	const flags = parseFlags(fileHex);
-	const mons = Array(20)
-		.fill(null)
-		.map(() => Array(20).fill(null));
-	for (let box = 0; box < 20; box++) {
-		for (let i = 0; i < 20; i++) {
-			if (indexes[box][i] === -1) continue;
-			const block =
-				(flags[box][i] === '0' ? '1' : '2') +
-				(indexes[box][i] < 167 ? 'A' : indexes[box][i] < 195 ? 'B' : 'C');
-			let address: number = addresses[`sBoxMons${block}` as keyof typeof addresses];
-			if (block[1] === 'A') {
-				address += indexes[box][i] * 49;
-			} else if (block[1] === 'B') {
-				address += (indexes[box][i] - 167) * 49;
-			} else {
-				address += (indexes[box][i] - 195) * 49;
-			}
-			mons[box][i] = address;
-		}
-	}
-	return mons;
+  const indexes = parseIndexes(fileHex);
+  const flags = parseFlags(fileHex);
+  const mons = Array(20)
+    .fill(null)
+    .map(() => Array(20).fill(null));
+  for (let box = 0; box < 20; box++) {
+    for (let i = 0; i < 20; i++) {
+      if (indexes[box][i] === -1) continue;
+      const block =
+        (flags[box][i] === '0' ? '1' : '2') +
+        (indexes[box][i] < 167 ? 'A' : indexes[box][i] < 195 ? 'B' : 'C');
+      let address: number = addresses[`sBoxMons${block}` as keyof typeof addresses];
+      if (block[1] === 'A') {
+        address += indexes[box][i] * 49;
+      } else if (block[1] === 'B') {
+        address += (indexes[box][i] - 167) * 49;
+      } else {
+        address += (indexes[box][i] - 195) * 49;
+      }
+      mons[box][i] = address;
+    }
+  }
+  return mons;
 }
 export default parseBoxAddresses;
 
 function parseIndexes(fileHex: string[]): number[][] {
-	const indexes = Array(20)
-		.fill(null)
-		.map(() => Array(20).fill(-1));
-	for (let box = 0; box < 20; box++) {
-		for (let i = 0; i < 20; i++) {
-			indexes[box][i] = parseInt(fileHex[addresses.sBackupNewBox1 + (33 * box + i)], 16) - 1;
-		}
-	}
-	return indexes;
+  const indexes = Array(20)
+    .fill(null)
+    .map(() => Array(20).fill(-1));
+  for (let box = 0; box < 20; box++) {
+    for (let i = 0; i < 20; i++) {
+      indexes[box][i] = parseInt(fileHex[addresses.sBackupNewBox1 + (33 * box + i)], 16) - 1;
+    }
+  }
+  return indexes;
 }
 
 function parseFlags(fileHex: string[]): string[][] {
-	const flags = Array(20)
-		.fill(null)
-		.map((): string[] => Array(20).fill(''));
-	for (let box = 0; box < 20; box++) {
-		//Grab the three relevant bytes
-		const flagString = parseInt(
-			fileHex[addresses.sBackupNewBox1 + 33 * box + 20] +
-				fileHex[addresses.sBackupNewBox1 + 33 * box + 21] +
-				fileHex[addresses.sBackupNewBox1 + 33 * box + 22],
-			16
-		)
-			.toString(2)
-			.padStart(24, '0');
-		//Get flag bit
-		for (let i = 0; i < 20; i++) {
-			flags[box][i] = flagString[Math.floor(i / 8) * 16 + 7 - i];
-		}
-	}
-	return flags;
+  const flags = Array(20)
+    .fill(null)
+    .map((): string[] => Array(20).fill(''));
+  for (let box = 0; box < 20; box++) {
+    //Grab the three relevant bytes
+    const flagString = parseInt(
+      fileHex[addresses.sBackupNewBox1 + 33 * box + 20] +
+      fileHex[addresses.sBackupNewBox1 + 33 * box + 21] +
+      fileHex[addresses.sBackupNewBox1 + 33 * box + 22],
+      16
+    )
+      .toString(2)
+      .padStart(24, '0');
+    //Get flag bit
+    for (let i = 0; i < 20; i++) {
+      flags[box][i] = flagString[Math.floor(i / 8) * 16 + 7 - i];
+    }
+  }
+  return flags;
 }
--- src/lib/parsers/boxes/forward/parseMon.ts
+++ src/lib/parsers/boxes/forward/parseMon.ts
@@ -7,138 +7,137 @@ import { getNature, hex2bin, readString } from '$lib/utils';
 import type { Mon } from '$lib/types';
 
 function parseMon(fileHex: string[], address: number, PF: 'polished' | 'faithful'): Mon {
-	//Byte #1, Byte #22: Species, Form
-	const byte22 = hex2bin(fileHex[address + 21]);
-	const dexNo = parseInt(byte22.at(2)! + hex2bin(fileHex[address]), 2);
-	let formNo = parseInt(byte22.slice(3), 2);
-	//Form Number Zero TODO
-	if (formNo === 0) formNo = 1;
-	const species = pokemon[PF][dexNo];
-	const form = pokemon[PF][dexNo].forms.find((f) => f.formNo === formNo)!;
+  //Byte #1, Byte #22: Species, Form
+  const byte22 = hex2bin(fileHex[address + 21]);
+  const dexNo = parseInt(byte22.at(2)! + hex2bin(fileHex[address]), 2);
+  let formNo = parseInt(byte22.slice(3), 2);
+  //Form Number Zero TODO
+  if (formNo === 0) formNo = 1;
+  const species = pokemon[PF][dexNo];
+  const form = species.forms.find((f) => f.formNo === formNo)!;
+  //Byte #2: Held Item
+  let heldItem = 'None';
+  if (!(fileHex[address + 1] === '00')) {
+    heldItem = items[PF][parseInt(fileHex[address + 1], 16) - 1].name;
+  }
 
-	//Byte #2: Held Item
-	let heldItem = 'None';
-	if (!(fileHex[address + 1] === '00')) {
-		heldItem = items[PF][parseInt(fileHex[address + 1], 16) - 1].name;
-	}
+  //Bytes #3-#6: Moveset
+  const moveset: string[] = [];
+  for (let i = 2; i < 6; i++) {
+    if (fileHex[address + i] === '00') {
+      moveset.push('None');
+    } else {
+      moveset.push(moves[PF][parseInt(fileHex[address + i], 16) - 1].name);
+    }
+  }
 
-	//Bytes #3-#6: Moveset
-	const moveset: string[] = [];
-	for (let i = 2; i < 6; i++) {
-		if (fileHex[address + i] === '00') {
-			moveset.push('None');
-		} else {
-			moveset.push(moves[PF][parseInt(fileHex[address + i], 16) - 1].name);
-		}
-	}
+  //Bytes #7-#8: Original Trainer ID
+  const OTID = parseInt(fileHex[address + 6] + fileHex[address + 7], 16);
 
-	//Bytes #7-#8: Original Trainer ID
-	const OTID = parseInt(fileHex[address + 6] + fileHex[address + 7], 16);
+  //Bytes #9-#11: Experience
+  const exp = parseInt(fileHex[address + 8] + fileHex[address + 9] + fileHex[address + 10], 16);
 
-	//Bytes #9-#11: Experience
-	const exp = parseInt(fileHex[address + 8] + fileHex[address + 9] + fileHex[address + 10], 16);
+  //Bytes #12-#17: Effort Values
+  const evs = [];
+  for (let i = 11; i < 17; i++) {
+    evs.push(parseInt(fileHex[address + i], 16));
+  }
 
-	//Bytes #12-#17: Effort Values
-	const evs = [];
-	for (let i = 11; i < 17; i++) {
-		evs.push(parseInt(fileHex[address + i], 16));
-	}
+  //Bytes #18-#20: Determinant Values
+  const dvs = [];
+  for (let i = 0; i < 6; i++) {
+    dvs.push(parseInt(fileHex[address + 17 + Math.floor(i / 2)].at(i % 2)!, 16));
+  }
 
-	//Bytes #18-#20: Determinant Values
-	const dvs = [];
-	for (let i = 0; i < 6; i++) {
-		dvs.push(parseInt(fileHex[address + 17 + Math.floor(i / 2)].at(i % 2)!, 16));
-	}
+  //Bytes #21: Shininess, Ability, Nature
+  const byte21 = hex2bin(fileHex[address + 20]);
+  const shininess = byte21.at(0)! === '0' ? 'Not Shiny' : 'Shiny';
+  const ability = abilities[PF].find(
+    (ability) => form.abilities.at(parseInt(byte21.slice(1, 3), 2) - 1)! === ability.name
+  )!.name;
+  const nature = getNature(parseInt(byte21.slice(3), 2));
 
-	//Bytes #21: Shininess, Ability, Nature
-	const byte21 = hex2bin(fileHex[address + 20]);
-	const shininess = byte21.at(0)! === '0' ? 'Not Shiny' : 'Shiny';
-	const ability = abilities[PF].find(
-		(ability) => form.abilities.at(parseInt(byte21.slice(1, 3), 2) - 1)! === ability.name
-	)!.name;
-	const nature = getNature(parseInt(byte21.slice(3), 2));
+  //Byte #22: Gender, isEgg
+  const gender = form.hasGender ? (byte22.at(0)! === '0' ? 'Male' : 'Female') : 'Genderless';
+  const isEgg = byte22.at(1)! === '0' ? false : true;
 
-	//Byte #22: Gender, isEgg
-	const gender = form.hasGender ? (byte22.at(0)! === '0' ? 'Male' : 'Female') : 'Genderless';
-	const isEgg = byte22.at(1)! === '0' ? false : true;
+  //Byte #23: PP UPs
+  const PPUPs = [];
+  for (let i = 3; i > -1; i--) {
+    PPUPs.push(parseInt(hex2bin(fileHex[address + 22]).slice(i * 2, i * 2 + 1), 2));
+  }
 
-	//Byte #23: PP UPs
-	const PPUPs = [];
-	for (let i = 3; i > -1; i--) {
-		PPUPs.push(parseInt(hex2bin(fileHex[address + 22]).slice(i * 2, i * 2 + 1), 2));
-	}
+  //Byte #24: Happiness / Hatch Cycles
+  const happiness = parseInt(fileHex[address + 23], 16);
 
-	//Byte #24: Happiness / Hatch Cycles
-	const happiness = parseInt(fileHex[address + 23], 16);
+  //Byte #25: Pokerus
+  const pokerus: { strain: number | 'None' | 'Cured'; daysRemaining: number } = {
+    strain: 'None',
+    daysRemaining: 0
+  };
+  const pokerusStr = hex2bin(fileHex[address + 24]).slice(4);
+  if (pokerusStr === '1101') {
+    pokerus.strain = 'Cured';
+  } else if (pokerusStr != '0000') {
+    pokerus.strain = pokerusStr.split('1').length - 1;
+    pokerus.daysRemaining = pokerus.strain - (4 - pokerusStr.replace(/^0+/, '').length);
+  }
 
-	//Byte #25: Pokerus
-	const pokerus: { strain: number | 'None' | 'Cured'; daysRemaining: number } = {
-		strain: 'None',
-		daysRemaining: 0
-	};
-	const pokerusStr = hex2bin(fileHex[address + 24]).slice(4);
-	if (pokerusStr === '1101') {
-		pokerus.strain = 'Cured';
-	} else if (pokerusStr != '0000') {
-		pokerus.strain = pokerusStr.split('1').length - 1;
-		pokerus.daysRemaining = pokerus.strain - (4 - pokerusStr.replace(/^0+/, '').length);
-	}
+  //Byte #26: Caught Time, Caught Ball
+  const byte26 = hex2bin(fileHex[address + 25]);
+  const caughtTime = ['Evening', 'Morning', 'Day', 'Night'][parseInt(byte26.slice(1, 3), 2)];
+  let caughtBall = 'Park Ball';
+  if (byte26.slice(3) != '00000') {
+    caughtBall = items[PF][parseInt(byte26.slice(3), 2) - 1].name;
+  }
 
-	//Byte #26: Caught Time, Caught Ball
-	const byte26 = hex2bin(fileHex[address + 25]);
-	const caughtTime = ['Evening', 'Morning', 'Day', 'Night'][parseInt(byte26.slice(1, 3), 2)];
-	let caughtBall = 'Park Ball';
-	if (byte26.slice(3) != '00000') {
-		caughtBall = items[PF][parseInt(byte26.slice(3), 2) - 1].name;
-	}
+  //Byte #27: Caught Level
+  const caughtLevel = parseInt(fileHex[address + 26], 16);
 
-	//Byte #27: Caught Level
-	const caughtLevel = parseInt(fileHex[address + 26], 16);
+  //Byte #28: Caught Location
+  const caughtLocation = locations[PF][parseInt(fileHex[address + 27], 16)].name;
 
-	//Byte #28: Caught Location
-	const caughtLocation = locations[PF][parseInt(fileHex[address + 27], 16)].name;
+  //Byte #29: Level
+  const level = parseInt(fileHex[address + 28], 16);
 
-	//Byte #29: Level
-	const level = parseInt(fileHex[address + 28], 16);
+  //Byte #30: Hyper Training
+  const hyperTraining = [...hex2bin(fileHex[address + 29])].map((stat) =>
+    stat === '1' ? true : false
+  );
 
-	//Byte #30: Hyper Training
-	const hyperTraining = [...hex2bin(fileHex[address + 29])].map((stat) =>
-		stat === '1' ? true : false
-	);
+  //Bytes #31-#32: Unused
 
-	//Bytes #31-#32: Unused
+  //Bytes #33-#42: Nickname
+  const nickname = readString(fileHex, address + 32, 10, true);
 
-	//Bytes #33-#42: Nickname
-	const nickname = readString(fileHex, address + 32, 10, true);
+  //Bytes #43-#49: Original Trainer Nickname
+  const OTNickname = readString(fileHex, address + 42, 7, true);
 
-	//Bytes #43-#49: Original Trainer Nickname
-	const OTNickname = readString(fileHex, address + 42, 7, true);
-
-	return {
-		species: species.name,
-		form: form.id,
-		heldItem,
-		moves: moveset,
-		OTID,
-		exp,
-		evs,
-		dvs,
-		shininess,
-		ability,
-		nature,
-		isEgg,
-		gender,
-		PPUPs,
-		happiness,
-		pokerus,
-		caughtBall,
-		caughtTime,
-		caughtLevel,
-		caughtLocation,
-		level,
-		hyperTraining,
-		nickname,
-		OTNickname
-	};
+  return {
+    species: species.name,
+    form: form.id,
+    heldItem,
+    moves: moveset,
+    OTID,
+    exp,
+    evs,
+    dvs,
+    shininess,
+    ability,
+    nature,
+    isEgg,
+    gender,
+    PPUPs,
+    happiness,
+    pokerus,
+    caughtBall,
+    caughtTime,
+    caughtLevel,
+    caughtLocation,
+    level,
+    hyperTraining,
+    nickname,
+    OTNickname
+  };
 }
 export default parseMon;
