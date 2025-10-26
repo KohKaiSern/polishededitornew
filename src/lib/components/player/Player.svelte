a6b38b652aa291fc949d426ee2c70e2fb3d5f996
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-27 00:24:29 +0800

    Change Gender to Model for Player

--- src/lib/components/player/Player.svelte
+++ src/lib/components/player/Player.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { Heading, Label } from 'flowbite-svelte';
-	import { NumberInput, RadioSelect, TextInput } from '../UI';
+	import { Heading, Img, Label, Radio } from 'flowbite-svelte';
+	import { NumberInput, TextInput } from '../UI';
 	import type { Player } from '$lib/types';
 
 	let { player = $bindable(), PF }: { player: Player; PF: 'polished' | 'faithful' } = $props();
@@ -16,20 +16,30 @@
 		<Label>ID</Label>
 		<NumberInput bind:value={player.id} min={0} max={65535} />
 	</div>
-	<div class="flex flex-col gap-2">
-		<Label>In-Game Model</Label>
-		<RadioSelect
-			bind:value={player.gender}
-			options={[
-				{ text: 'Chris', id: 'Male' },
-				{ text: 'Kris', id: 'Female' },
-				{ text: 'Crys', id: 'Non-Binary' }
-			]}
-		/>
-	</div>
+
 	<div class="flex flex-col gap-2">
 		<Label>Money</Label>
 		<NumberInput bind:value={player.money} min={0} max={999999} />
+	</div>
+</div>
+<div class="flex flex-col gap-2 mt-3">
+	<Label>In-Game Model</Label>
+	<div class="flex gap-5 mt-2">
+		{#each ['Chris', 'Kris', 'Crys'] as model, i}
+			<Radio
+				custom
+				bind:group={player.gender}
+				value={{ Chris: 'Male', Kris: 'Female', Crys: 'Non-Binary' }[model]}
+			>
+				<div
+					class="bg-white size-fit p-2 border-1 rounded-lg border-gray-300 dark:border-gray-600 peer-checked:border-transparent peer-checked:ring-4 peer-checked:ring-primary-300"
+				>
+					<Img
+						src={`https://raw.githubusercontent.com/KohKaiSern/polishededitor/refs/heads/main/src/gfx/${model.toLowerCase()}.png`}
+					/>
+				</div>
+			</Radio>
+		{/each}
 	</div>
 </div>
 
