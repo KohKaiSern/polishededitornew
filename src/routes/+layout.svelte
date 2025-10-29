35e3ac2561241f84dfe53179aa14f860c8d95fe2
Koh Kai Sern <kaisern.koh@gmail.com>
2025-10-29 14:04:36 +0800

    Enable Speed Insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import favicon from '$lib/assets/favicon.svg';
 	import '../app.css';
 
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
 	let { children } = $props();
+
+	injectSpeedInsights();
 </script>
 
 <svelte:head>
