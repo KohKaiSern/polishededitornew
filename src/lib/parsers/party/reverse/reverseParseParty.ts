d07fe5d9fb86b55b4697eeb8d48f152bd6442852
Koh Kai Sern <kaisern.koh@gmail.com>
2025-11-02 16:02:07 +0800

    Wait no this should actually fix it now

--- src/lib/parsers/party/reverse/reverseParseParty.ts
+++ src/lib/parsers/party/reverse/reverseParseParty.ts
@@ -4,21 +4,26 @@ import type { PartyMon } from '$lib/types';
 import reverseParsePartyMon from './reverseParsePartyMon';
 
 function reverseParseParty(
-	fileHex: string[],
-	party: PartyMon[],
-	PF: 'polished' | 'faithful'
+  fileHex: string[],
+  party: PartyMon[],
+  PF: 'polished' | 'faithful'
 ): string[] {
-	const address = addresses.sBackupPokémonData + 8;
-	fileHex[address - 8] = party.filter(Boolean).length.toString(16).padStart(2, '0');
-	for (let i = 0; i < 6; i++) {
-		if (!party[i]) continue;
-		fileHex = reverseParsePartyMon(fileHex, address + 48 * i, party[i], PF);
-		fileHex = writeString(fileHex, address + 288 + i * 11, 8, party[i].OTNickname, false);
-		fileHex[address + 307 + i * 11] = bin2hex(
-			party[i].hyperTraining.map((stat) => (stat ? '1' : '0')).join('') + '00'
-		);
-		fileHex = writeString(fileHex, address + 354 + i * 11, 11, party[i].nickname, false);
-	}
-	return fileHex;
+  const address = addresses.sBackupPokémonData + 8;
+  fileHex[address - 8] = party.filter(Boolean).length.toString(16).padStart(2, '0');
+  for (let i = 0; i < 6; i++) {
+    if (!party[i]) continue;
+    fileHex = reverseParsePartyMon(fileHex, address + 48 * i, party[i], PF);
+    //TODO: TempFix: Apostrophe
+    if (party[i].OTNickname.length < 8) {
+      fileHex = writeString(fileHex, address + 288 + i * 11, 8, party[i].OTNickname, false);
+    }
+    fileHex[address + 307 + i * 11] = bin2hex(
+      party[i].hyperTraining.map((stat) => (stat ? '1' : '0')).join('') + '00'
+    );
+    if (party[i].nickname.length < 11) {
+      fileHex = writeString(fileHex, address + 354 + i * 11, 11, party[i].nickname, false);
+    }
+  }
+  return fileHex;
 }
 export default reverseParseParty;
