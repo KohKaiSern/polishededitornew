a5f89b69dcfa94f075fb3b1e1d5e36942ebd17a9
Koh Kai Sern <kaisern.koh@gmail.com>
2025-11-06 08:18:53 +0800

    Possibly Fixed Party Hyper Training?

--- src/extractors/addresses.ts
+++ src/extractors/addresses.ts
@@ -2,63 +2,66 @@ import { splitRead } from './utils';
 
 //Symbols
 const SYMBOLS = [
-	'sSaveVersion',
-	'sBackupNewBox1',
-	'sBoxMons1A',
-	'sBoxMons1B',
-	'sBoxMons1C',
-	'sBoxMons2A',
-	'sBoxMons2B',
-	'sBoxMons2C',
-	'sBackupPlayerData',
-	'sBackupChecksum',
-	'sChecksum',
-	'sGameData',
-	'sGameDataEnd',
-	'sBackupGameData',
-	'sBackupGameDataEnd',
-	'sBackupPokémonData',
-	'wNumItems',
-	'wNumMedicine',
-	'wNumBalls',
-	'wNumBerries',
-	'wTMsHMs',
-	'wKeyItems',
-	'wCoins',
-	'wApricorns',
-	'wWingAmounts',
-	'wCandyAmounts',
-	'wBlueCardBalance',
-	'wPlayerID',
-	'wPlayerGender',
-	'wPlayerName',
-	'wRivalName',
-	'wMoney',
-	'wBadges',
-	'wGameData'
+  'sSaveVersion',
+  'sBackupNewBox1',
+  'sBoxMons1A',
+  'sBoxMons1B',
+  'sBoxMons1C',
+  'sBoxMons2A',
+  'sBoxMons2B',
+  'sBoxMons2C',
+  'sBackupPlayerData',
+  'sBackupChecksum',
+  'sChecksum',
+  'sGameData',
+  'sGameDataEnd',
+  'sBackupGameData',
+  'sBackupGameDataEnd',
+  'sBackupPokémonData',
+  'wNumItems',
+  'wNumMedicine',
+  'wNumBalls',
+  'wNumBerries',
+  'wTMsHMs',
+  'wKeyItems',
+  'wCoins',
+  'wApricorns',
+  'wWingAmounts',
+  'wCandyAmounts',
+  'wBlueCardBalance',
+  'wPlayerID',
+  'wPlayerGender',
+  'wPlayerName',
+  'wRivalName',
+  'wMoney',
+  'wBadges',
+  'wPartyMonOTs',
+  'wPartyMon1HyperTraining',
+  'wPartyMon1Nickname',
+  'wGameData',
 ];
 
 //Converts wRAM address to sRAM
 function wToSRAM(address: string): number {
-	//M = (0x2000 * PP) + (QQQQ - 0xA000), where the original memory address was PP:QQQQ
-	return 8192 * parseInt(address.slice(0, 2), 16) + (parseInt(address.slice(2), 16) - 40960);
+  //M = (0x2000 * PP) + (QQQQ - 0xA000), where the original memory address was PP:QQQQ
+  return 8192 * parseInt(address.slice(0, 2), 16) + (parseInt(address.slice(2), 16) - 40960);
 }
 
 function makeAddress(addresses: Record<string, number>): Record<string, number> {
-	for (const [symbol, address] of Object.entries(addresses)) {
-		if (symbol.startsWith('s')) continue;
-		addresses[symbol] = address + addresses.sBackupGameData - addresses.wGameData;
-	}
-	return addresses;
+  for (const [symbol, address] of Object.entries(addresses)) {
+    if (symbol.startsWith('s')) continue;
+    addresses[symbol] = address + addresses.sBackupGameData - addresses.wGameData;
+  }
+  return addresses;
 }
 
 function extractAddresses(ADDRESSES: string[]): Record<string, number> {
-	const addresses: Record<string, number> = {};
-	for (const entry of SYMBOLS) {
-		const symbol = ADDRESSES.find((line) => line.endsWith(entry))!;
-		addresses[entry] = wToSRAM(symbol.split(' ').at(0)!.replace(':', ''));
-	}
-	return addresses;
+  const addresses: Record<string, number> = {};
+  for (const entry of SYMBOLS) {
+    const symbol = ADDRESSES.find((line) => line.endsWith(entry))!;
+    addresses[entry] = wToSRAM(symbol.split(' ').at(0)!.replace(':', ''));
+  }
+  return addresses;
 }
 
 const ADDRESSES = splitRead('../polishedcrystal.sym');
--- src/lib/parsers/party/forward/parseParty.ts
+++ src/lib/parsers/party/forward/parseParty.ts
@@ -4,16 +4,16 @@ import type { PartyMon } from '$lib/types';
 import parsePartyMon from './parsePartyMon';
 
 function parseParty(fileHex: string[], PF: 'polished' | 'faithful'): PartyMon[] {
-	const party = Array(6).fill(null);
-	const address = addresses.sBackupPokémonData + 8;
-	for (let i = 0; i < parseInt(fileHex[address - 8], 16); i++) {
-		party[i] = parsePartyMon(fileHex, address + 48 * i, PF);
-		party[i]['OTNickname'] = readString(fileHex, address + 288 + i * 11, 7, false);
-		party[i]['hyperTraining'] = [...hex2bin(fileHex[address + 307 + i * 11]).slice(0, 6)].map(
-			(stat) => (stat === '1' ? true : false)
-		);
-		party[i]['nickname'] = readString(fileHex, address + 354 + i * 11, 10, false);
-	}
-	return party;
+  const party = Array(6).fill(null);
+  const address = addresses.sBackupPokémonData + 8;
+  for (let i = 0; i < parseInt(fileHex[address - 8], 16); i++) {
+    party[i] = parsePartyMon(fileHex, address + 48 * i, PF);
+    party[i]['OTNickname'] = readString(fileHex, addresses.wPartyMonOTs + i * 11, 7, false);
+    party[i]['hyperTraining'] = [...hex2bin(fileHex[addresses.wPartyMon1HyperTraining + i * 11]).slice(0, 6)].map(
+      (stat) => (stat === '1' ? true : false)
+    );
+    party[i]['nickname'] = readString(fileHex, addresses.wPartyMon1Nickname + i * 11, 10, false);
+  }
+  return party;
 }
 export default parseParty;
